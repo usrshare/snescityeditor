37443be6287134cf0019e955f984eb0d9b388bbe
uls6502@tuta.io <usr_share>
2016-10-02 21:46:23 +0300

    trying to do something with the windows ui

--- src/win_ui.c
+++ src/win_ui.c
@@ -6,6 +6,24 @@ int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLin
 
 int win_ui_main(void) {
 
+	HMODULE hInstance = GetModuleHandle(NULL);
+
+	char filename[MAX_PATH];
+
+	char* openFilters[] = {
+		"SNES SRAM files","*.srm\0\0"};
+
+	OPENFILENAME x = {
+
+		.lStructSize = sizeof x,
+		.hInstance = hInstance,
+		.lpstrFilter = openFilters,
+		.lpstrFile = filename,
+		.nMaxFile = MAX_PATH,
+	};
+	
+	int r = GetOpenFileName(&x);
+
 	MessageBoxA(0,"Windows UI not implemented yet.","Sorry",0);
 	return 0;
 }
